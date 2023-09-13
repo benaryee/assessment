6dcc36b76c34cc2cfa90958d1ce17aa2056cba35
bernard <bernardaryee4@gmail.com>
2023-09-13 06:46:52 +0000

    Changed dev postgres connection properties

--- src/main/java/com/spektra/assessment/service/TransactionService.java
+++ src/main/java/com/spektra/assessment/service/TransactionService.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public interface TransactionService {
 
+    //TODO: Pageable Request
     List<Transaction> getAllTransactions();
 
     Transaction getTransaction(Long id);
